f4d0babb264e4de9b1bee559c5060636a29d4977
William Koller <wkoller25@gmail.com>
2020-02-22 16:36:55 -0300

    fix

--- rest/routes/pedidos.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const PedidosController = require('../controllers/pedidos-controller');
-
-router.get('/', PedidosController.getPedidos);
-router.post('/', PedidosController.postPedidos);
-router.get('/:id_pedido', PedidosController.getOnePedido);
-router.delete('/', PedidosController.deletePedidos);
-module.exports = router;
--- rest/routes/usuarios.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const UsuarioController = require('../controllers/usuarios-controller');
-router.post('/cadastro', UsuarioController.postCadastro);
-router.post('/login', UsuarioController.postLogin);
-
-module.exports = router;
